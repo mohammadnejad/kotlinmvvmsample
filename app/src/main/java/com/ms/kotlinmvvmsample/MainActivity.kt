a330c460534aea49f750037cdd0ac6d1a5af538d
mohammadnejad <majidrasht@gmail.com>
2018-05-21 18:49:32 +0430

    # change style

--- app/src/main/java/com/ms/kotlinmvvmsample/MainActivity.kt
+++ app/src/main/java/com/ms/kotlinmvvmsample/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.ms.kotlinmvvmsample
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import com.ms.kotlinmvvmsample.core.extension.replaceFragmentInActivity
 import com.ms.kotlinmvvmsample.core.extension.setActionBar
 import com.ms.kotlinmvvmsample.home.HomeFragment
+import com.ms.kotlinmvvmsample.location.LocationFragment
+import com.ms.kotlinmvvmsample.setting.SettingFragment
+import kotlinx.android.synthetic.main.activity_main_content.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +21,17 @@ class MainActivity : AppCompatActivity() {
             setDisplayShowHomeEnabled(false)
         }
 
-        replaceFragmentInActivity(HomeFragment.newInstance(), R.id.frameLayout)
+        bottomNavigation.setOnSelectedItemChangeListener {
+            when (it) {
+                R.id.tabHome -> replaceFragment(HomeFragment.newInstance())
+                R.id.tabSetting -> replaceFragment(SettingFragment.newInstance())
+                R.id.tabLocation -> replaceFragment(LocationFragment.newInstance())
+            }
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        replaceFragmentInActivity(fragment, R.id.frameLayout)
     }
 }
